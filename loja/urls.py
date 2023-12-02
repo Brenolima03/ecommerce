b1dee7261d96c79f049a890e477a5328de6a2316
root <root@DESKTOP-4IKO78M>
2023-12-02 15:38:12 -0400

    Fixing urls that redirected to detalhe

--- loja/urls.py
+++ loja/urls.py
@@ -4,16 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # Debug Toolbar URL
-    path("__debug__/", include("debug_toolbar.urls")) if settings.DEBUG else None,
-
-    # Admin URLs
     path('admin/', admin.site.urls),
-
-    # Your other app URLs
     path('', include('produto.urls')),
     path('perfil/', include('perfil.urls')),
     path('pedido/', include('pedido.urls')),
+    # Remover
+    path("__debug__/", include("debug_toolbar.urls")) if settings.DEBUG else None,
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
